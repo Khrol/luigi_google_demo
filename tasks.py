54707adf6774d8b8880e4023fbdf474a79bdca4d
Igor Khrol <igor.khrol@toptal.com>
2017-10-11 21:23:52 +0300

    Cleanup a bit.

--- tasks.py
+++ tasks.py
@@ -7,15 +7,7 @@ class Developers(TableTask):
 
     def main(self):
         developers = [
-            {'id': 10, 'name': 'Vasya', 'country_id': 1},
-            {'id': 11, 'name': 'Kolya', 'country_id': 1},
-            {'id': 12, 'name': 'Mikola', 'country_id': 2},
-            {'id': 13, 'name': 'Luda', 'country_id': 2},
-            {'id': 14, 'name': 'Alex', 'country_id': 2},
-            {'id': 15, 'name': 'Grisha', 'country_id': 2},
-            {'id': 16, 'name': 'Leh', 'country_id': 3},
-            {'id': 17, 'name': 'Paul', 'country_id': 3},
-            {'id': 18, 'name': 'Jan', 'country_id': 3},
+            {'id': 10, 'name': 'Igor', 'country_id': 1},
         ]
         return pd.DataFrame(developers)
 
@@ -26,8 +18,6 @@ class Countries(TableTask):
     def main(self):
         countries = [
             {'id': 1, 'name': 'Belarus'},
-            {'id': 2, 'name': 'Ukraine'},
-            {'id': 3, 'name': 'Poland'},
         ]
         return pd.DataFrame(countries)
 
@@ -38,7 +28,7 @@ class DevelopersByCountries(TableTask):
     def requires(self):
         return {
             'developers': Developers(),
-            'countries': Countries()
+            'countries': Countries(),
         }
 
     def main(self, developers, countries):
